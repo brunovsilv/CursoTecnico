34291f9b3af1e60309233848bf71a0a22c2006de
brunovsilv <169068441+brunovsilv@users.noreply.github.com>
2024-05-13 23:28:36 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Curso Tecnico");
+    System.out.println("");
+    System.out.println(" Bom, vou falar o que estou achando do curdo, acho ele bem complexo e detalhado ele e meio difícil mas ao longo do tempo devo em acustumar.              Ao longo do tempo acho que irá ficar mais dificl, mas se prestar bem atenção e se acustumar com as coisas.                                Tenho que me esforçar porque sei que não e fácil, então e se prestar bem atenção em tudo, não desfocar das coisas.                                   E por fim quero concluir elogiando esse curso, pois poed abrir muitas janelas e empregos bem alto qualificado.");
+  }
+}
